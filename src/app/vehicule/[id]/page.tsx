4a358d39ab9089ec12c84e58cf986a4b9834c886
DashOTB <digitaldashotb@gmail.com>
2025-06-06 01:40:40 +0200

    Update page.tsx

--- src/app/vehicule/[id]/page.tsx
+++ src/app/vehicule/[id]/page.tsx
@@ -38,7 +38,7 @@ interface Vehicle {
   caution: number;
 }
 
-export default function VehiclePage({ params }: { params: { id: string } }) {
+export default function VehiclePage({ params }: any) {
   const [step, setStep] = useState<BookingStep>("calendar");
   const [dateRange, setDateRange] = useState<[Date | null, Date | null]>([null, null]);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
